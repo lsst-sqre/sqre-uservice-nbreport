03d7bc88a16e24da42c4bcdae9c9dbc97a12b2ed
Jonathan Sick <jsick@lsst.org>
2018-07-27 10:15:17 -0700

    Add requires_github_org_membership decorator
    This decorated tests that an authenticated user is a member of the
    configured GitHub organization, and therefore authorized to access
    the decorated endpoint.
    
    Uses the AUTHORIZED_GITHUB_ORG configuration

--- uservice_nbreport/auth.py
+++ uservice_nbreport/auth.py
@@ -1,15 +1,18 @@
 """Authentication with GitHub.
 """
 
-__all__ = ('github_token_auth',)
+__all__ = ('github_token_auth', 'requires_github_org_membership')
 
-from flask import g
+from functools import wraps
+
+from flask import g, current_app
 from flask_httpauth import HTTPBasicAuth
 
 import requests
 import structlog
+from apikit import BackendError
 
-from .exceptions import ValidationError
+from .exceptions import GitHubAuthenticationError, GitHubAuthorizationError
 
 
 github_token_auth = HTTPBasicAuth()
@@ -44,8 +47,11 @@ def verify_github_token(username, token):
        key.
 
     2. The data structured returned by the ``GET https://api.github.com/user``
-       call is attached to ``flask.g`` under the ``github_user_data``
-       attribute.
+       call is attached to ``flask.g.github_user_data``
+
+    3. The GitHub username is attached to ``flask.github_user``.
+
+    4. The GitHub token is attached to ``flask.github_token``.
     """
     if username is None:
         return False
@@ -75,8 +81,86 @@ def unauthorized_token_access():
     """Callback for when the `github_token_auth` authentication fails that
     emits a 401 status and json-formatted error message.
     """
-    raise ValidationError(
-        'Please authenticate with a GitHub username and personal access '
-        'token.',
-        status_code=401
+    raise GitHubAuthenticationError()
+
+
+def requires_github_org_membership():
+    """Test GitHub organization membership (to be used as a route decorator).
+    """
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            try:
+                verify_github_org_membership()
+                return f(*args, **kwargs)
+
+            except (GitHubAuthenticationError, GitHubAuthorizationError):
+                raise
+
+            except Exception as e:
+                raise BackendError(
+                    'Unexpected server error during authentication.',
+                    status_code=500,
+                    content=str(e)
+                )
+        return decorated_function
+    return decorator
+
+
+def verify_github_org_membership():
+    """Verify that the authenticated GitHub user is a member of the
+    GitHub organization configured in the ``AUTHORIZED_GITHUB_ORG`` config.
+
+    This function is used by `requires_github_org_membership`.
+
+    Raises
+    ------
+    GitHubAuthenticationError
+        Raised if GitHub authentication has not occured.
+    GitHubOrgAuthorizationError
+        Raised if the user is not a member of the required GitHub
+        organization.
+    """
+    try:
+        username = g.github_user
+        token = g.github_token
+    except AttributeError:
+        raise GitHubAuthenticationError()
+
+    # Access the user's organization memberships (need to iterate)
+    # https://developer.github.com/v3/orgs/#list-your-organizations
+    org_data = iter_github_endpoint(
+        'https://api.github.com/user/orgs',
+        auth=(username, token),
+        headers={'Accept': 'application/vnd.github.v3+json'}
     )
+
+    org_list = [org['login'] for org in org_data]
+    if current_app.config['AUTHORIZED_GITHUB_ORG'] not in org_list:
+        raise GitHubAuthorizationError()
+
+
+def iter_github_endpoint(url, verb='GET', auth=None, headers=None,
+                         current_data=None):
+    """Perform a request and follow GitHub-style pagination.
+    """
+    if current_data is None:
+        current_data = []
+
+    response = requests.request(
+        verb,
+        url,
+        auth=auth,
+        headers=headers)
+    response.raise_for_status()
+
+    current_data.extend(response.json())
+
+    try:
+        next_url = response.links['next']['url']
+    except (AttributeError, KeyError):
+        return current_data
+
+    return iter_github_endpoint(
+        next_url, verb=verb, auth=auth, headers=headers,
+        current_data=current_data)
--- uservice_nbreport/exceptions.py
+++ uservice_nbreport/exceptions.py
@@ -20,3 +20,34 @@ class ValidationError(BackendError):
     def __str__(self):
         return "ValidationError: {0:d} {1} [{2}]".format(
             self.status_code, self.reason, self.content)
+
+
+class GitHubAuthenticationError(ValidationError):
+    """Exception raised for GitHub authentication issues.
+    """
+
+    def __init__(self, reason=None, content=None):
+        if reason is None:
+            reason = ('Please authenticate using a GitHub username and '
+                      'personal access token with Basic auth.')
+        super().__init__(reason, status_code=401, content=content)
+
+    def __str__(self):
+        return "GitHubAuthenticationError: {0:d} {1} [{2}]".format(
+            self.status_code, self.reason, self.content)
+
+
+class GitHubAuthorizationError(ValidationError):
+    """Exception raised if the user is not a member of the specified GitHub
+    organization for authorization.
+    """
+
+    def __init__(self, reason=None, content=None):
+        if reason is None:
+            reason = ('You are not a member of the specified GitHub '
+                      'organization for authorization.')
+        super().__init__(reason, status_code=403, content=content)
+
+    def __str__(self):
+        return "GitHubAuthorizationError: {0:d} {1} [{2}]".format(
+            self.status_code, self.reason, self.content)
