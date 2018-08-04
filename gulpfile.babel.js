d8d979cdbc699eacbc780dbe87e2714ed8a53161
Jonathan Sick <jsick@lsst.org>
2018-08-03 18:42:41 -0700

    Add sass gulp task
    This lets us write CSS for the report as Sass, and compile it into the
    template's directory in the package.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,28 @@
+import cleanCSS from 'gulp-clean-css';
 import gulp from 'gulp';
 import minimist from 'minimist';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
 
 // parse command line options
 // [--env dev (default) | prod]
 const options = minimist(process.argv);
 const env = options.env || 'dev';
+
+// Environment-based configurations for CleanCss
+// https://github.com/jakubpawlowicz/clean-css
+const cleanCssConfig = {
+  dev: {
+    compatibility: '*',
+    level: 2,
+    format: 'beautify'
+  },
+
+  prod: {
+    compatibility: '*',
+    level: 2,
+  }
+};
 
 /*
  * gulp environment
@@ -13,7 +31,39 @@ const env = options.env || 'dev';
 export const environment = () => console.log(`${env}`);
 
 /*
- * gulp
- * Default task.
+ * gulp sass
+ * Compile the sass
  */
-export default environment;
+const sassTask = () => {
+  let stream = gulp.src('scss/app.scss')
+    // Initialize sourcemaps
+    .pipe(sourcemaps.init())
+    // Compile sass synchronously
+    .pipe(sass.sync().on('error', sass.logError))
+    // Clean CSS
+    .pipe(cleanCSS(cleanCssConfig[env]));
+
+  if (env === 'dev') {
+    // Write out sourcemaps
+    stream.pipe(sourcemaps.write());
+  }
+
+  stream.pipe(gulp.dest('uservice_nbreport/publish/templates/report-html'));
+  return stream;
+};
+gulp.task('sass', sassTask);
+
+/*
+ * gulp watch
+ * Watch for source changes and rebuild any assets
+ */
+const watchTask = () => {
+  gulp.watch("styles.scss", ['sass']);
+};
+gulp.task('watch', watchTask);
+
+/*
+ * gulp
+ * Default task that compiles assets and watches for additional changes.
+ */
+gulp.task('default', ['sass', 'watch']);
